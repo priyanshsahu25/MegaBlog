cc080c846f59e948dd70ebd4e6f00889abc13b62
Priyansh Sahu <priyansh.sahu25@gmail.com>
2024-03-10 20:05:28 +0530

    Commit

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {useDispatch} from 'react-redux'
 import authService from '../../appwrite/auth'
-
+import { useNavigate} from 'react-router-dom'
 
 import {logout} from '../../store/authSlice'
 
@@ -9,13 +9,15 @@ import {logout} from '../../store/authSlice'
 function LogoutBtn() {
 
   const [loggedin,setLoggedin]=useState(true);
+  const navigate = useNavigate()
     const dispatch = useDispatch()
     const logoutHandler = () => {
     
         authService.logout().then(() => {
           setLoggedin(false)
             dispatch(logout())
-  
+            navigate("/login")
+            
          
         })
 
